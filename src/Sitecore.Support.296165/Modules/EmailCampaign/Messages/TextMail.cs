6e1fd23ba743638ee4ebbd3462835183e25d8755
DK\anr <anr@sitecore.net>
2018-11-29 11:41:55 +0200

    import code

---
+++ src/Sitecore.Support.296165/Modules/EmailCampaign/Messages/TextMail.cs
@@ -0,0 +1,62 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Messages
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Modules.EmailCampaign.Core.Personalization;
+  using Sitecore.Modules.EmailCampaign.Factories;
+  using Sitecore.Modules.EmailCampaign.Services;
+  using Sitecore.EmailCampaign.Model.Message;
+  public class TextMail : Sitecore.Modules.EmailCampaign.Messages.TextMail
+  {
+    private PersonalizationManager _personalizationManager;
+
+    protected TextMail(Item item, IMessageItemSourceFactory messageItemSourceFactory, IManagerRootService managerRootService) : base(item, messageItemSourceFactory, managerRootService)
+    {
+    }
+    public new static TextMail FromItem(Item item, IMessageItemSourceFactory messageItemSourceFactory, IManagerRootService managerRootService)
+    {
+      if (!IsCorrectMessageItem(item))
+      {
+        return null;
+      }
+      return new TextMail(item, messageItemSourceFactory, managerRootService);
+    }
+
+    protected override PersonalizationManager PersonalizationManager
+    {
+      get
+      {
+        if (_personalizationManager == null)
+        {
+          _personalizationManager = new PersonalizationManager();
+          if (CustomPersonTokens.Count > 0)
+          {
+            var mapper = new DictionaryTokenMapper();
+
+            foreach (var token in CustomPersonTokens)
+            {
+              if (!string.IsNullOrWhiteSpace(token.Key) && token.Value != null)
+              {
+                mapper.BindToken(new Token(token.Key), token.Value);
+              }
+            }
+
+            _personalizationManager.AddTokenMapper(mapper);
+          }
+          if (PersonalizationRecipient != null)
+          {
+            var mapper = new RecipientPropertyTokenMapper(PersonalizationRecipient);
+            _personalizationManager.AddTokenMapper(mapper);
+          }
+          else if (MessageType == MessageType.Automated && Context.User != null)
+          {
+            // Assume we are in the context of a "Quick test" or Message tab preview
+            var mapper = new ContextUserTokenMapper();
+            _personalizationManager.AddTokenMapper(mapper);
+          }
+        }
+        return this._personalizationManager;
+      }
+      set { _personalizationManager = value; }
+    }
+  }
+}
---
+++ src/Sitecore.Support.296165/TypeResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Modules.EmailCampaign.Core.HostnameMapping;
+using Sitecore.Modules.EmailCampaign.Factories;
+using Sitecore.Modules.EmailCampaign.Messages;
+using Sitecore.Modules.EmailCampaign.Services;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core
+{
+  public class TypeResolver : Sitecore.Modules.EmailCampaign.Core.TypeResolver
+  {
+
+    private readonly IMessageItemSourceFactory _messageItemSourceFactory;
+    private readonly IManagerRootService _managerRootService;
+    public TypeResolver(IHostnameMappingService hostnameMappingService, IMessageItemSourceFactory messageItemSourceFactory, IManagerRootService managerRootService, IMultiVariateTestStrategyFactory multiVariateTestStrategyFactory, IAbnTestService abnTestService)
+      : base(hostnameMappingService, messageItemSourceFactory, managerRootService, multiVariateTestStrategyFactory, abnTestService)
+    {
+      _messageItemSourceFactory = messageItemSourceFactory;
+      _managerRootService = managerRootService;
+    }
+
+    public override MessageItem GetCorrectMessageObject(Item item)
+    {
+      if (item != null && TextMail.IsCorrectMessageItem(item))
+        return Sitecore.Support.Modules.EmailCampaign.Messages.TextMail.FromItem(item, _messageItemSourceFactory, _managerRootService);
+      return base.GetCorrectMessageObject(item);
+    }
+  }
+}
